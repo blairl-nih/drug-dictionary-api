c3fd238cf54d678346cb6c43f7d62ec7088be1ad
Blair Learn <blairl-nih@users.noreply.github.com>
2020-05-05 17:21:14 -0400

    Set up initial project skeleton.

---
+++ src/NCI.OCPL.Api.DrugDictionary/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using NCI.OCPL.Api.Common;
+using NCI.OCPL.Api.DrugDictionary.Models;
+
+namespace NCI.OCPL.Api.DrugDictionary
+{
+    public class Startup : NciStartupBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:NCI.OCPL.Api.DrugDictionary.Startup"/> class.
+        /// </summary>
+        /// <param name="env">Env.</param>
+        public Startup(IHostingEnvironment env)
+            : base(env) { }
+
+
+        /*****************************
+         * ConfigureServices methods *
+         *****************************/
+
+        /// <summary>
+        /// Adds the configuration mappings.
+        /// </summary>
+        /// <param name="services">Services.</param>
+        protected override void AddAdditionalConfigurationMappings(IServiceCollection services)
+        {
+        }
+
+        /// <summary>
+        /// Adds in the application specific services
+        /// </summary>
+        /// <param name="services">Services.</param>
+        protected override void AddAppServices(IServiceCollection services)
+        {
+            // Add our Query Service
+            services.Configure<DrugDictionaryAPIOptions>(Configuration.GetSection("DrugDictionaryAPI"));
+        }
+
+        /*****************************
+         *     Configure methods     *
+         *****************************/
+
+        /// <summary>
+        /// Configure the specified app and env.
+        /// </summary>
+        /// <returns>The configure.</returns>
+        /// <param name="app">App.</param>
+        /// <param name="env">Env.</param>
+        /// <param name="loggerFactory">Logger.</param>
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        protected override void ConfigureAppSpecific(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            return;
+        }
+    }
+}
